5834cec5d59e2323ffba2ca365a481c5e8987e4a
lincoder <718681050@qq.com>
2018-07-16 15:03:54 +0800

    Initial commit

---
+++ src/datatype/Cachepool.java
@@ -0,0 +1,13 @@
+package datatype;
+
+public class Cachepool {
+
+    public static void main(String[] args) {
+        Integer x = new Integer(123);
+        Integer y = new Integer(123);
+        System.out.println(x == y);    // false
+        Integer z = Integer.valueOf(1222);
+        Integer k = Integer.valueOf(1222);
+        System.out.println(z == k);   //true
+    }
+}
