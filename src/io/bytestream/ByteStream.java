de85fbd1d27988d2d850da8ebbe5f8b31e7670d5
lincoder <718681050@qq.com>
2018-07-17 17:40:50 +0800

    javaI/O

---
+++ src/io/bytestream/ByteStream.java
@@ -0,0 +1,13 @@
+package io.bytestream;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+
+public class ByteStream {
+
+    public static void main(String[] args) {
+        FilterInputStream stream;
+        BufferedInputStream bufferedInputStream;
+    }
+}
