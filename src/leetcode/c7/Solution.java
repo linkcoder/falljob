be1bf8f6033b94e9e848b68862fc0002f88ac7e8
lincoder <718681050@qq.com>
2018-07-23 16:12:03 +0800

    leetcode练习

---
+++ src/leetcode/c7/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.c7;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public int reverse(int x) {
+        ArrayList<Integer> list=new ArrayList<>();
+        boolean positive=x>=0?true:false;
+        x=Math.abs(x);
+        while(x!=0){
+            list.add(x%10);
+            x=x/10;
+        }
+        long result=0;
+        for(int e:list){
+            result=result*10+e;
+        }
+        result=positive?result:-result;
+        if(result< Integer.MIN_VALUE||result>Integer.MAX_VALUE){
+            result=0;
+        }
+        return (int)result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+
+        System.out.println(solution.reverse(1555555555));
+    }
+}
