87bc1328e1dc5f5b7e7be2379f860a61b5e539af
lincoder <718681050@qq.com>
2018-07-27 16:02:54 +0800

    Integer的小陷阱

---
+++ src/base/data/IntegerJdk.java
@@ -0,0 +1,12 @@
+package base.data;
+
+public class IntegerJdk {
+
+    public static void main(String[] args) {
+        Integer e1=new Integer(1);
+        Integer e2=new Integer(2);
+
+        System.out.println(e1==e2);
+        System.out.println(e1==1);
+    }
+}
