24bb94cfa999bdf9e77d1e1a3d5a980107742cf8
lincoder <718681050@qq.com>
2018-07-27 17:43:32 +0800

    Abstract注意

--- src/base/data/FloatJdk.java
+++ src/base/data/FloatJdk.java
@@ -4,7 +4,6 @@ public class FloatJdk {
 
     public static void main(String[] args) {
         float x=2.2f;
-
         short y=1;
         y= (short) (y+1);
         y+=1;
--- src/base/datatype/Cachepool.java
+++ src/base/datatype/Cachepool.java
@@ -14,6 +14,8 @@ public class Cachepool {
 //        String
 //        StringBuilder
 //        StringBuffer
+
+//        Object
     }
 
 }
