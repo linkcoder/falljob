2e3df6fde575596df6f72ebb6f41e2946a632eea
lincoder <718681050@qq.com>
2018-07-28 15:24:19 +0800

    set源码了解

--- src/container/collection/SetJDK.java
+++ src/container/collection/SetJDK.java
@@ -9,7 +9,11 @@ public class SetJDK {
 
     public static void main(String[] args) {
         Set set1=new HashSet();
-        Set set2=new TreeSet();
+        set1.add(null);//可以添加null
         Set set3=new LinkedHashSet();
+        set3.add(null);//可以添加null
+        Set set2=new TreeSet();
+        set2.add(null);//不可以，不可以添加null，抛异常，因为null没有comparable
+
     }
 }
