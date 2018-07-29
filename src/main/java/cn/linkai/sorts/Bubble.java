bf907dc7ea12ec2f465d31dc283eee0e92ca2487
lincoder <718681050@qq.com>
2018-07-29 22:33:39 +0800

    冒泡排序

--- src/main/java/cn/linkai/sorts/Bubble.java
+++ src/main/java/cn/linkai/sorts/Bubble.java
@@ -1,9 +1,26 @@
 package cn.linkai.sorts;
 
 public class Bubble<T extends Comparable<T>> extends Sort<T> {
-
+    /**
+     * 冒泡排序
+     * 时间复杂度：N^2
+     * 空间复杂度: 1
+     */
     @Override
     public T[] sort(T[] nums) {
-        return null;
+        int length=nums.length;
+        boolean hasSorted=false;
+
+        for (int i = 0; i <length && !hasSorted ; i++) {
+            hasSorted=true;//优化，当没有出现交换的时候，则说明已经有序了
+            for (int j =0;j<length-i-1;j++){
+                if(less(nums[j+1],nums[j])){
+                    swap(nums,j,j+1);
+                    hasSorted=false;
+                }
+            }
+            printEveryResult(nums);
+        }
+        return nums;
     }
 }
--- src/test/java/cn/linkai/SortTest.java
+++ src/test/java/cn/linkai/SortTest.java
@@ -1,5 +1,7 @@
 package cn.linkai;
 
+import cn.linkai.sorts.Bubble;
+import cn.linkai.sorts.Insert;
 import cn.linkai.sorts.Selection;
 import org.junit.Test;
 
@@ -10,5 +12,21 @@ public class SortTest {
         Integer[] nums={2,5,3,1,4,6,0,8};
         Selection<Integer> selection=new Selection<>();
         selection.sort(nums);
+    }
+
+
+    @Test
+    public void testBubble(){
+        Integer[] nums={2,5,3,1,4,6,0,8};
+        Bubble<Integer> bubble=new Bubble<>();
+        bubble.sort(nums);
+    }
+
+
+    @Test
+    public void testInsert(){
+        Integer[] nums={2,5,3,1,4,6,0,8};
+        Insert<Integer> insert=new Insert<>();
+        insert.sort(nums);
     }
 }
