ca6a503f1b4566457ba6f0d047c0c87ae90a756d
lincoder <718681050@qq.com>
2018-07-29 22:33:49 +0800

    插入排序

---
+++ src/main/java/cn/linkai/sorts/Insert.java
@@ -0,0 +1,21 @@
+package cn.linkai.sorts;
+
+public class Insert <T extends Comparable<T>> extends Sort<T>{
+    /**
+     * 插入排序
+     * 时间复杂度：N^2
+     * 空间复杂度：1
+     * 次数：N
+     */
+    @Override
+    public T[] sort(T[] nums) {
+        int len=nums.length;
+        for(int i=1;i<len;i++){
+            for(int j=i;j>0 && less(nums[j],nums[j-1]);j--){
+                swap(nums,j,j-1);
+            }
+            printEveryResult(nums);
+        }
+        return nums;
+    }
+}
