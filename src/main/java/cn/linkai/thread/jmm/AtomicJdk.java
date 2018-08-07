498f8c2b731149c41d226bd35ec5665d6d50a3da
lincoder <718681050@qq.com>
2018-08-07 17:39:50 +0800

    补充原子类和sychronized的场景

--- src/main/java/cn/linkai/thread/jmm/AtomicJdk.java
+++ src/main/java/cn/linkai/thread/jmm/AtomicJdk.java
@@ -9,5 +9,6 @@ public class AtomicJdk {
         //里面全部调用了Unsafe类的cas方法来保证修改的原子性
         // setup to use Unsafe.compareAndSwapInt for updates
         AtomicInteger in=new AtomicInteger(1);
+        //volatile不能保证原子性的原因是因为多个线程的load assign 和 store的操作合起来不是原子性
     }
 }
