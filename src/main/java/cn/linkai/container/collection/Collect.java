c56a37cd5a728e3530078f0f722ca5724eeedbb4
lincoder <718681050@qq.com>
2018-08-07 17:52:13 +0800

    补充原子类和sychronized的场景

---
+++ src/main/java/cn/linkai/container/collection/Collect.java
@@ -0,0 +1,12 @@
+package cn.linkai.container.collection;
+
+import java.util.Collections;
+
+public class Collect {
+
+    public static void main(String[] args) {
+//        Collections.synchronizedList();
+
+        //synchronizedCollection是将原来的集合进行封装，加上sychronized进行同步
+    }
+}
