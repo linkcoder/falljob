f6cd46bd3c0ddf6a73c1806a4e8e191ad7d21624
lincoder <718681050@qq.com>
2018-08-16 14:16:18 +0800

    旋转数组

--- src/main/java/cn/linkai/pointoffer/s7/Solution.java
+++ src/main/java/cn/linkai/pointoffer/s7/Solution.java
@@ -1,5 +1,7 @@
 package cn.linkai.pointoffer.s7;
 
+import java.util.ArrayList;
+
 public class Solution {
 
     public int minNumberInRotateArray(int [] array) {
@@ -27,6 +29,29 @@ public class Solution {
 
     }
 
+    public int minNumberInRotateArray2(int [] array) {
+        if(array==null || array.length==0) return 0;
+
+        int minIndex=findMINIndex(array);
+        int min=array[minIndex];
+
+        if(minIndex==0) return min;
+
+        int i=0;
+        ArrayList<Integer> nums=new ArrayList<>();
+        while(i<array.length){
+            nums.add(array[minIndex%array.length]);
+            minIndex++;
+            i++;
+        }
+
+        for(int j=0;j<nums.size();j++){
+            array[j]=nums.get(j);
+        }
+        return min;
+
+    }
+
     private int findMINIndex(int[] array) {
         int minIndex=0;
         for(int i=0;i<array.length;i++){
@@ -39,7 +64,7 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution=new Solution();
         int[] arr={3,4,5,1,2};
-        int min = solution.minNumberInRotateArray(arr);
+        int min = solution.minNumberInRotateArray2(arr);
         System.out.println(min);
         for(int e:arr)
             System.out.println(e);
