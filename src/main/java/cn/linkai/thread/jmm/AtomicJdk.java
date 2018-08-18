9c4c5479df4a793aa11f4ac27fc6bac7f897504b
lincoder <718681050@qq.com>
2018-08-18 10:59:41 +0800

    补充线程基础

--- src/main/java/cn/linkai/thread/jmm/AtomicJdk.java
+++ src/main/java/cn/linkai/thread/jmm/AtomicJdk.java
@@ -11,5 +11,7 @@ public class AtomicJdk {
         AtomicInteger in=new AtomicInteger(1);
         //volatile不能保证原子性的原因是因为多个线程的load assign 和 store的操作合起来不是原子性
         //sychronized使用的是lock和unlock操作，来锁主存的变量，所以能保证las三步的原子性
+        //cas缺点：
+        //1.循环开销；2.只能同步一个变量；3.ABA问题（如果这种问题采用互斥同步性能较高）
     }
 }
