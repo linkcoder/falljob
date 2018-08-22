cba3a4a6384f374131c0f0b0cc3e75e930e65ebf
lincoder <718681050@qq.com>
2018-08-22 16:00:36 +0800

    字符串的排列（类型LeetCode的组合总数）引自Cyc

---
+++ src/main/java/cn/linkai/pointoffer/s28/Solution.java
@@ -0,0 +1,36 @@
+package cn.linkai.pointoffer.s28;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    //引自Cyc https://github.com/CyC2018/CS-Notes
+    private ArrayList<String> ret = new ArrayList<>();
+
+    public ArrayList<String> Permutation(String str) {
+        if (str.length() == 0)
+            return ret;
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        backtracking(chars, new boolean[chars.length], new StringBuilder());
+        return ret;
+    }
+
+    private void backtracking(char[] chars, boolean[] hasUsed, StringBuilder s) {
+        if (s.length() == chars.length) {
+            ret.add(s.toString());
+            return;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (hasUsed[i])
+                continue;
+            if (i != 0 && chars[i] == chars[i - 1] && !hasUsed[i - 1]) /* 保证不重复 */
+                continue;
+            hasUsed[i] = true;
+            s.append(chars[i]);
+            backtracking(chars, hasUsed, s);
+            s.deleteCharAt(s.length() - 1);
+            hasUsed[i] = false;
+        }
+    }
+}
