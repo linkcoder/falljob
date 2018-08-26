cc33b209df1f446586f5bf446ffb1052526e04c5
lincoder <718681050@qq.com>
2018-08-26 15:53:54 +0800

    表示数值的字符串

---
+++ src/main/java/cn/linkai/pointoffer/s54/Solution.java
@@ -0,0 +1,10 @@
+package cn.linkai.pointoffer.s54;
+
+public class Solution {
+
+    public boolean isNumeric(char[] str) {
+        if (str == null || str.length == 0)
+            return false;
+        return new String(str).matches("[+-]?\\d*(\\.\\d+)?([eE][+-]?\\d+)?");
+    }
+}
