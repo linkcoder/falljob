7913c514c495289dbc215d29cd3e90f4b9e939b1
lincoder <718681050@qq.com>
2018-08-26 15:57:41 +0800

    字符流中第一个不重复的字符

---
+++ src/main/java/cn/linkai/pointoffer/s55/Solution.java
@@ -0,0 +1,21 @@
+package cn.linkai.pointoffer.s55;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    private int[] cnts = new int[256];
+    private Queue<Character> queue = new LinkedList<>();
+
+    public void Insert(char ch) {
+        cnts[ch]++;
+        queue.add(ch);
+        while (!queue.isEmpty() && cnts[queue.peek()] > 1)
+            queue.poll();
+    }
+
+    public char FirstAppearingOnce() {
+        return queue.isEmpty() ? '#' : queue.peek();
+    }
+}
