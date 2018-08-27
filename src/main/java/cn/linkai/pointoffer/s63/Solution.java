77d3cd9f43ad49bacca129f83734746117c3efde
lincoder <718681050@qq.com>
2018-08-27 15:51:35 +0800

    二叉搜索树的第K个节点（中序遍历）

---
+++ src/main/java/cn/linkai/pointoffer/s63/Solution.java
@@ -0,0 +1,39 @@
+package cn.linkai.pointoffer.s63;
+
+public class Solution {
+
+    int count=0;
+
+    TreeNode KthNode(TreeNode pRoot, int k) {
+
+        if(pRoot!=null){
+            TreeNode node=KthNode(pRoot.left,k);
+            if(node!=null) return node;
+            count++;
+            if(count==k)
+                return pRoot;
+            node=KthNode(pRoot.right,k);
+            if(node!=null)
+                return node;
+        }
+
+        return null;
+    }
+
+
+
+
+}
+
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
