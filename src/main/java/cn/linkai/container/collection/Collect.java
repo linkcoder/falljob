23e6c0a9723390685b995b9fe49ef4e461db0b3b
lincoder <718681050@qq.com>
2018-09-03 20:33:12 +0800

    补充基础（注意Arrays）

--- src/main/java/cn/linkai/container/collection/Collect.java
+++ src/main/java/cn/linkai/container/collection/Collect.java
@@ -1,11 +1,13 @@
 package cn.linkai.container.collection;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Collect {
 
     public static void main(String[] args) {
-        //Arrays.asList(T)基本类型不可以泛型化，如果传入基本类型的数组，是直接生成一个数组的列表
+//        Arrays.asList(T)基本类型不可以泛型化，如果传入基本类型的数组，是直接生成一个数组的列表
+        //同时Arrays里面自己实现了ArrayList，所以是不可变的列表
 //        Collections.synchronizedList();
 
         //synchronizedCollection是将原来的集合进行封装，加上sychronized进行同步
