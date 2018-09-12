dfb39f4624173004c935bc6bdcd79f98c4c99a55
lincoder <718681050@qq.com>
2018-09-12 22:39:39 +0800

    重复的子字符串（难理解待）

---
+++ src/main/java/cn/linkai/leetcode/c459/Solution.java
@@ -0,0 +1,21 @@
+package cn.linkai.leetcode.c459;
+
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int l = s.length();
+        int[] next = new int[l];
+        next[0] = -1;
+        int i, j = -1;
+        for (i = 1; i < l; i++) {
+            while (j >= 0 && s.charAt(i) != s.charAt(j + 1)) {
+                j = next[j];
+            }
+            if (s.charAt(i) == s.charAt(j + 1)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        int lenSub = l - 1 - next[l - 1];
+        return lenSub != l && l % lenSub ==0;
+    }
+}
